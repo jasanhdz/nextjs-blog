d418c582d696947c88bce19d0c405ff12a1868fa
Jasan Hernández <jasan814@gmail.com>
2021-09-14 13:07:52 -0500

    [add] query at api by name, tags and userName

--- src/pages/api/notion/books/index.js
+++ src/pages/api/notion/books/index.js
@@ -1,9 +1,57 @@
 import { Client } from '@notionhq/client'
+import { capitalize } from '../../../../utils/strings'
 
 export default async function(req, res) {
-  const notion = new Client({ auth: process.env.NOTION_API_KEY })
-  const response = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID,
-  })
-  res.status(200).json({ ...response });
+  const args = { database_id: process.env.NOTION_DATABASE_ID }
+  const { search = '' } = req.query
+  try {
+    const notion = new Client({ auth: process.env.NOTION_API_KEY })
+    if(search) {
+      const usersResponse = await notion.users.list({ page_size: 5 })
+      const idUser = getUserIdByName(usersResponse.results, search)
+      console.log(idUser)
+      args.filter = getQueryByNameAndTag(search)
+      if(idUser) args.filter.or.push({ property: 'user', people: { contains: idUser }})
+    }
+    const response = await notion.databases.query(args)
+    res.status(200).json({ ...response });
+  } catch (error) {
+    console.log(error)
+    res.status(503).json({ error });
+  }
 }
+
+function getUserIdByName(users = [], name = '') {
+  let idUser = null
+  for(let i = 0; i < users.length; i++) {
+    if(users[i].name.includes(name) || users[i].name.includes(capitalize(name))) {
+      idUser = users[i].id
+      i = users.length
+    }
+  }
+  return idUser
+}
+
+
+const getQueryByNameAndTag = (search) => ({
+  or: [
+    {
+      property: 'Name',
+      text: {
+        contains: search,
+      },
+    },
+    {
+      property: 'Name',
+      text: {
+        contains: capitalize(search),
+      },
+    },
+    {
+      property: 'Tags',
+      multi_select: {
+        contains: search
+      }
+    },
+  ]
+})
--- src/services/posts.js
+++ src/services/posts.js
@@ -1,10 +1,12 @@
 import 'isomorphic-fetch'
-// const PATH = 'https://blog-jasanhdz.vercel.app'
-const PATH = 'http://localhost:3000'
+const PATH = 'https://blog-jasanhdz.vercel.app'
+// const PATH = 'http://localhost:3000'
 
-export async function getBooks() {
+export async function getBooks(query) {
+  const baseUrl = `${PATH}/api/notion/books`
+  const url = query ? baseUrl.concat(`?search=${query}`) : baseUrl
   try {
-    const response = await fetch(`${PATH}/api/notion/books`)
+    const response = await fetch(url)
     if (response.status >= 400) return { statusCode: response.status }
     const data = await response.json()
     return { data, statusCode: response.status }
